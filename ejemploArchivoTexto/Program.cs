c83a182fd8da224d7323d835904cc79248694a50
jvelazquez <jonathanvgms@gmail.com>
2019-06-12 22:47:17 -0300

    Ejemplo lectura archivos texto

--- ejemploArchivoTexto/Program.cs
+++ ejemploArchivoTexto/Program.cs
@@ -14,7 +14,8 @@ namespace ejemploArchivoTexto
             // string variable = "algo\mas algo"  -> INCORRECTO
             string directorio = @"C:\Users\Jonathan\Desktop\source\";
 
-            
+            //======================================================================
+
             // Ejemplo #1: Escribir un vector de strings al archivo
             
             // Creo un vector de strings que posee cuatro lineas
@@ -23,7 +24,8 @@ namespace ejemploArchivoTexto
             // El método WriteAllLines crea un archivo y escribe la colección de strings en el archivo y lo cierra luego. 
             // No es necesario llamar a los métodos Flush() o Close()
             File.WriteAllLines(directorio + "archivoLineasDeTexto.txt", lines);
-            
+
+            //======================================================================
 
             // Ejemplo #2: Escribir una cadena de texto al archivo
 
@@ -33,7 +35,8 @@ namespace ejemploArchivoTexto
             // No es necesario llamar a los métodos Flush() o Close()
             File.WriteAllText(directorio + "archivoConString.txt", text);
 
-            
+            //======================================================================
+
             // Ejemplo #3: Escribir unicamente algunas lineas del vector de strings al archivo
 
             // La instrucción using automaticamente vacia el buffer, cierra el stream y llama al método IDisposable.Dispose sobre el objeto stream object
@@ -48,14 +51,60 @@ namespace ejemploArchivoTexto
                     }
                 }
             }
-            
 
-            // Ejemplo #4: Anexa (Append) nuevo texto a un archivo de texto existente.
+            //======================================================================
+
+            // Ejemplo #4: Anexa (Append) nuevo texto a un archivo de texto existente
 
             // La instrucción using automaticamente vacia el buffer, cierra el stream y llama al método IDisposable.Dispose sobre el objeto stream object
             using (StreamWriter file = new StreamWriter(directorio + "archivoLineasDeTexto2.txt", true))
             {
                 file.WriteLine("última linea");
+            }
+
+            //======================================================================
+
+            // Ejemplo #5: Leer todo el contenido del archivo de texto en una sola linea
+
+            // El método ReadAllText abre el archivo para lectura, lee todo el contenido y lo asigna a la variable contenido, luego cierra el archivo.
+            string contenido = File.ReadAllText(directorio + "archivoLineasDeTexto.txt");
+
+            Console.WriteLine($"Contenido: {contenido} Cantidad caracteres: {contenido.Length}");
+
+            Console.WriteLine("=========================================");
+
+            //======================================================================
+
+            // Ejemplo #6: Leer todas las lineas del archivo de texto
+
+            // El método ReadAllLines lee el contenido del archivo linea por linea y retorna un vector de string donde cada posicion represnta una linea de texto.
+            string[] lineas = File.ReadAllLines(directorio + "archivoLineasDeTexto.txt");
+
+            foreach (string linea in lineas)
+            {
+                Console.WriteLine($"Contenido: {linea} Cantidad caracteres: {linea.Length}");
+            }
+
+            Console.WriteLine("=========================================");
+            //======================================================================
+
+            // Ejemplo #7: Leer el archivo linea por linea
+
+            // El método ReadLine lee una linea de caracteres mientras no llegue al final del archivo.
+            using (StreamReader file = new StreamReader(directorio + "archivoLineasDeTexto.txt"))
+            {
+                int contador = 0;
+                string linea = String.Empty;
+
+                while ((linea = file.ReadLine()) != null)
+                {
+                    Console.WriteLine($"Contenido: {linea} Cantidad caracteres: {linea.Length}");
+                    contador++;
+                }
+
+                file.Close();
+
+                Console.WriteLine($"El archivo tienes {contador} lineas.");
             }
         }
     }
